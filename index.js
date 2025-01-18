f144725a454088932d0595784b6013ca79cea56f
Ali Eltahan <alyaltahan@gmail.com>
2025-01-18 06:43:59 +0200

    Simulating blocking thread.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+})
+
+app.get('/non-blocking', (req, res) => {
+	res.status(200).send('This page is non-blocking');
+})
+
+app.get('/blocking', async (req, res) => {
+	let counter = 0
+	for (let i = 1; i <= 1_000_000; i++) {
+		counter ++
+	}
+	res.status(200).send('blocking' + `Counter is ${counter}`);
+});
+

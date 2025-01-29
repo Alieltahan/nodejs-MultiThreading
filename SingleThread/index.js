da75dfa343d7e886da0658661e85ac3abc749d4f
Ali Eltahan <alyaltahan@gmail.com>
2025-01-29 08:26:08 +0200

    Update single thread

--- SingleThread/index.js
+++ SingleThread/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const { Worker } = require('worker_threads');
 
 app.use(express.json());
 const port = process.env.PORT || 3000;
@@ -13,13 +12,10 @@ app.get('/non-blocking', (req, res) => {
 })
 
 app.get('/blocking', async (req, res) => {
-	const worker = new Worker('./SingleThread/worker.js');
-	worker.on('message', (message) => {
-		res.status(200).send(`Blocking - ${message}`);
-	})
-
-	worker.on('error', (err) => {
-		res.status(400).send(`Error--: ${err.message}`);
-	})
+	let counter = 0
+	for (let i = 1; i <= 20_000_000_000; i++) {
+		counter ++
+	}
+		res.status(200).send(`Blocking - ${counter}`);
 });
 
--- SingleThread/worker.js
+++
@@ -1,8 +0,0 @@
-const { parentPort } = require('worker_threads');
-
-let counter = 0
-for (let i = 1; i <= 20_000_000_000; i++) {
-	counter ++
-}
-
-parentPort.postMessage(counter);
